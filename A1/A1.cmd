a0c6d1393cfed78586d8c3e66f9c659b7edfd062
James Cooper <jarak_jakar@hotmail.com>
2016-03-18 21:00:27 +1300

    Created batch script to compile with library options specified - path relative to source files, based on VS structure

---
+++ A1/A1.cmd
@@ -0,0 +1,1 @@
+call fsc.exe --lib:..\packages\Akka.1.0.6\lib\net45 --lib:..\packages\Akka.FSharp.1.0.6\lib\net45 --reference:Akka.dll --reference:Akka.FSharp.dll -o:A1.exe Program.fs
---
+++ A1/Scripts/load-references-debug.fsx
@@ -0,0 +1,7 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../packages/Akka.1.0.6/lib/net45/Akka.dll"
+#r "../../packages/Newtonsoft.Json.8.0.3/lib/net45/Newtonsoft.Json.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
