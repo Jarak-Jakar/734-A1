7d845d361519de5503a017f67d68b93b86770f7c
James Cooper <jarak_jakar@hotmail.com>
2016-03-22 11:03:43 +1300

    Got program reading in strings from two .txt files

--- A1/Program.fs
+++ A1/Program.fs
@@ -4,8 +4,18 @@
 open Akka
 open Akka.FSharp
 
+let findLCSLenSeq (inputStringOne: List<char>) (inputStringTwo: List<char>) upperBoundaryList leftBoundaryList = 
+    inputStringOne.Length
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     printfn "Hello world!"
+    printfn "%A" argv.[2]
+    //0 // return an integer exit code
+    let stringOne = System.IO.File.ReadAllText(argv.[0].[4..]).ToCharArray() |> Array.toList
+    let stringTwo = System.IO.File.ReadAllText(argv.[1].[4..]).ToCharArray() |> Array.toList
+    //printfn "%A" stringOne
+    //printfn "%A" stringTwo
+    printfn "%A" (findLCSLenSeq stringOne stringTwo 3 4)
     0 // return an integer exit code
---
+++ Linqpad A1 attempt.linq
@@ -0,0 +1,14 @@
+<Query Kind="FSharpProgram">
+  <Reference Relative="packages\Akka.1.0.6\lib\net45\Akka.dll">C:\Misc\James\Computer Science\CS 734\734-A1\packages\Akka.1.0.6\lib\net45\Akka.dll</Reference>
+  <Reference Relative="packages\Akka.FSharp.1.0.6\lib\net45\Akka.FSharp.dll">C:\Misc\James\Computer Science\CS 734\734-A1\packages\Akka.FSharp.1.0.6\lib\net45\Akka.FSharp.dll</Reference>
+</Query>
+
+open Akka
+open Akka.FSharp
+
+[<EntryPoint>]
+//let main argv = 
+  //  printfn "%A" argv
+    //printfn "Hello world!"
+    //0 // return an integer exit code
+    
