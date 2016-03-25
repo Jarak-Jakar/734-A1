6bf7f5bc9cb961ee3ac111a4410fde3679dad70f
James Cooper <jarak_jakar@hotmail.com>
2016-03-25 22:21:30 +1300

    Explored creation/initialisation of arrays.

--- A1/Program.fs
+++ A1/Program.fs
@@ -10,41 +10,17 @@ let checkCell firstChar secondChar top left topLeft =
     else (firstChar, secondChar, (max top left))
 
 let findLCSLenSeq (topString: array<char>) (sideString: array<char>) (topValues: array<int>) (sideValues: array<int>) = 
-    (*let vectorOne = Array.zeroCreate(topString.Length)
-    let vectorTwo = Array.zeroCreate(topString.Length)
-    let vectorThree = Array.zeroCreate(topString.Length)*)
+    (*let vectorOne = Array2D.create topString.Length 3 0
+    let vectorTwo = Array2D.create topString.Length 3 0
+    let vectorThree = Array2D.create topString.Length 3 0*)
 
-    //Arrays first part of array is position in the vector, second is x co-ord, y co-ord, cell value
-    (*let vectorOne = Array2D.zeroCreate topString.Length 3
-    let vectorTwo = Array2D.zeroCreate topString.Length 3
-    let vectorThree = Array2D.zeroCreate topString.Length 3*)
+    let vectorOne = Array.create topString.Length (Some([|0; 0; 0|]))
+    let vectorTwo = Array.create topString.Length (Some([|0; 0; 0|]))
+    let vectorThree = Array.create topString.Length (Some([|0; 0; 0|]))
 
-    let vectorOne = Array2D.init topString.Length 3 (fun x y -> None)
-    let vectorTwo = Array2D.init topString.Length 3 (fun x y -> None)
-    let vectorThree = Array2D.init topString.Length 3 (fun x y -> None)
+    //Initialise vectors
+    
 
-    //Initialise vector values
-    //vectorOne.[0] <- (topIndex, sideIndex, topValues)
-   (* vectorOne.[0] <- (0, 0, topValues.[1])
-    vectorTwo.[0] <- (0, -1, 0)
-    vectorTwo.[1] <- (-1, 0, 0)
-    vectorThree.[0] <- (-1, -1, 0) *)
-
-    //Initialise vectors two and three
-    vectorTwo.[0, 0] <- 0
-    vectorTwo.[0, 1] <- 1
-    vectorTwo.[0, 2] <- topValues.[1]
-    vectorTwo.[1, 0] <- 1
-    vectorTwo.[1, 1] <- 0
-    vectorTwo.[1, 2] <- sideValues.[1]
-    vectorThree.[0, 0] <- 0
-    vectorThree.[0, 1] <- 0
-    vectorThree.[0, 2] <- sideValues.[0]
-
-    Array2D.iter (fun x -> Some(x)) vectorTwo.[2.., *]
-
-    //First step
-    //vectorOne.GetValue(0)
     64
 
 
